0b6b868f7a9f738938f5fe02e1c102e6020eac64
Christian Speckner <christian.speckner@mayflower.de>
2026-04-01 21:31:38 +0200

    Support windows build.

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -2,7 +2,10 @@ fn main() {
     println!("cargo:rerun-if-changed=network-backend/");
     tauri_build::build();
 
-    cc::Build::new()
+    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
+
+    let mut c_build = cc::Build::new();
+    c_build
         .files(&[
             "network-backend/nanopb/pb_common.c",
             "network-backend/nanopb/pb_decode.c",
@@ -10,10 +13,16 @@ fn main() {
             "network-backend/proto/networking.pb.c",
         ])
         .include("network-backend/nanopb/")
-        .include("network-backend/proto/")
-        .compile("network_backend_c");
+        .include("network-backend/proto/");
 
-    cc::Build::new()
+    if target_os == "windows" {
+        c_build.define("_WIN32_WINNT", "0x0600");
+    }
+
+    c_build.compile("network_backend_c");
+
+    let mut cpp_build = cc::Build::new();
+    cpp_build
         .cpp(true)
         .std("c++17")
         .files(&[
@@ -24,11 +33,18 @@ fn main() {
         ])
         .include("network-backend/")
         .include("network-backend/nanopb/")
-        .include("network-backend/proto/")
-        .compile("network_backend_cpp");
+        .include("network-backend/proto/");
 
-    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
+    if target_os == "windows" {
+        cpp_build.define("_WIN32_WINNT", "0x0600");
+    }
+
+    cpp_build.compile("network_backend_cpp");
+
     if target_os == "macos" {
         println!("cargo:rustc-link-lib=resolv");
+    } else if target_os == "windows" {
+        println!("cargo:rustc-link-lib=ws2_32");
+        println!("cargo:rustc-link-lib=iphlpapi");
     }
 }
